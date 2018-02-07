02c81e7122baddca591482286e0fc0caf826b2ac
buchio17 <buchio17@gmail.com>
2018-02-07 17:47:17 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  # before_action :require_user_logged_in, only: %i(index show)
+  # %w(some thing)
   
   def index
     @users = User.all.page(params[:page])
@@ -27,6 +29,19 @@ class UsersController < ApplicationController
     end
     
   end
+  
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
   
   private
   
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,6 @@
 module SessionsHelper
   def current_user
-    if @current_user
-      return @current_user
-    else
-      @current_user = User.find_by(id: session[:user_id])
-      return @current_user
-    end
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
