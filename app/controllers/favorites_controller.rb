ac2b58bbf051717f2e5957c46db34b57ac2e14a8
buchio17 <buchio17@gmail.com>
2018-02-17 07:34:58 +0000

    fix okiniiri delete process

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,13 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    current_user.add_favorite(params[:micropost_id])
+    current_user.add_favorite(params[:id])
     flash[:success] = 'お気に入りに登録しました。'
     redirect_to root_url
   end
 
   def destroy
-    current_user.delete_favorite(params[:micropost_id])
+    current_user.delete_favorite(params[:id])
     flash[:success] = 'お気に入りから削除しました。'
     redirect_to root_url
   end
